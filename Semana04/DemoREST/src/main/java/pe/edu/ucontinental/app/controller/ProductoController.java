f8a880af2872cdd54f88f8443cd5a72d8b5d5d9b
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2023-11-11 10:35:00 -0500

    Semana 4

--- Semana04/DemoREST/src/main/java/pe/edu/ucontinental/app/controller/ProductoController.java
+++ Semana04/DemoREST/src/main/java/pe/edu/ucontinental/app/controller/ProductoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +27,7 @@ public class ProductoController {
 	@Autowired
 	private ProductoService productoService;
 
+	@CrossOrigin(origins = "http://localhost:5050")
 	@GetMapping
 	public List<ProductoDto> getAll() {
 		return productoService.getAll();
